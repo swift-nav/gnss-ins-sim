6ddcf7907b7264fb594a2ef80cd94940caef29aa
Niels Joubert <njoubert@gmail.com>
2019-03-22 04:10:18 -0700

    batched parallel sims for the number of CPU cores available

--- swift_analysis/parallelsim.sh
+++ swift_analysis/parallelsim.sh
@@ -1,19 +1,41 @@
 #!/bin/bash -x
 
-N=500
+N=1000
 DUR=10
+CORES=`getconf _NPROCESSORS_ONLN`
 
-for ODOM in 'none' 'perfect'
+# This will be a little inefficient, but we will launch in batches 
+# then wait for the entire batch to finish
+
+EPOCH=`date +%s`
+ROOTDIR=results-$EPOCH
+
+mkdir -p $ROOTDIR
+
+i=0
+for TRAJ in 'CVSL'
 do
-	for VIBRATION in 'none' 'smooth'
+	for ODOM in 'none' 'nhc' 'perfectwheelodom'
 	do
-		for IMU in 'bmi160' 'bmw'
+		for VIBRATION in 'smooth' 'bumpy'
 		do
-			for SPEED in 12 33 55 112
+			for IMU in 'bmi160' 'had300' 'bmw_typ' 'bmw_max'  
 			do
-				OUTDIR="results-${IMU}-${SPEED}-${VIBRATION}-${ODOM}"
-				sleep 2
-				time python ./generate_dr_trajectories.py --outdir=$OUTDIR --N=$N --dur=$DUR --speed=$SPEED --imu=$IMU --vibrations=$VIBRATION --odometry=$ODOM &
+				for SPEED in 55 112
+				do
+					i=$((i+1))
+					b=$((i%CORES))
+					ipp=`printf "%05d" $i`
+					OUTDIR="$ROOTDIR/results-${ipp}-${IMU}-${SPEED}-${VIBRATION}-${ODOM}-${TRAJ}"
+					python ./generate_dr_trajectories.py --outdir=$OUTDIR --N=$N --dur=$DUR --speed=$SPEED --imu=$IMU --vibrations=$VIBRATION --dynamics=$ODOM --traj=$TRAJ >> $ROOTDIR/sim$ipp.log &
+					echo $! >> $ROOTDIR/sim$ipp.pid
+					sleep 1
+					if [ $b -eq 0 ]
+					then
+						echo "Waiting for batch of ${CORES} jobs to complete..."
+						wait
+					fi
+				done
 			done
 		done
 	done
